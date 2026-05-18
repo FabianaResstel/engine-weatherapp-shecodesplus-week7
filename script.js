e9817f9b85534587e98a238ae4ae85d09673f19c
FabianaResstel <fabianaresstel01@gmail.com>
2026-05-18 10:24:55 -0400

    injected forecast api and adjusted css

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function changeDataHtml(response) {
   let icon = document.querySelector("#weather-icon-response");
 
   icon.innerHTML = `<img src = "${response.data.condition.icon_url}" class="weather-icon" />`;
-  h1.innerHTML = response.data.city;
+  h1.innerHTML = `${response.data.city}, ${response.data.country}`;
   timeElement.innerHTML = formatDate(date);
   weatherTemperatureElement.innerHTML = `${Math.round(
     response.data.temperature.current,
@@ -21,6 +21,7 @@ function changeDataHtml(response) {
   weatherForecastElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${Math.round(response.data.temperature.humidity)}%`;
   windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -71,8 +72,38 @@ function searchForm(event) {
 
   apiElement(city.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = `0d9d6fa642662e53t328bfec1ado0b77`;
+  let apiUrlForecast = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+  axios(apiUrlForecast).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecast = document.querySelector("#forecast");
+  forecast.innerHTML = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecast.innerHTML += `<div class="forecast-day">
+                <div class="forecast-weekday">${formatDay(day.time)}</div>
+                <div "> <img src="${day.condition.icon_url}"class="forecast-icon />
+                </div>
+                <div class="forecast-min-temperature">${Math.round(day.temperature.maximum)}°</div>
+                <div class="forecast-max-temperature">${Math.round(day.temperature.minimum)}°</div>
+            </div>`;
+    }
+  });
+}
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchForm);
 
-apiElement("Vitoria Brasil");
+apiElement("brasilia");
